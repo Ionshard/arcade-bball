83ffa60bc328eb4447950b75b3b3056e76cd2747
Victor Ling <86419+Ionshard@users.noreply.github.com>
2025-12-09 10:11:29 -0500

    Get DHT11 kinda working

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 #![no_std]
 #![no_main]
 
-use arduino_hal::{adc, prelude::*};
+use core::fmt::Debug;
+
+use arduino_hal::prelude::*;
+use arduino_hal::adc;
+use embedded_dht_rs::dht11::Dht11;
 use panic_halt as _;
 use ufmt::{uwriteln};
 
@@ -40,14 +44,34 @@ fn main() -> ! {
 
     let a0 = pins.a0.into_analog_input(&mut adc);
 
+
+    let dht11_pin = pins.d2.into_opendrain_high();
+    let delay = arduino_hal::Delay::new();
+    let mut dht11 = Dht11::new(dht11_pin, delay);
+
     loop {
         led.toggle();
-        arduino_hal::delay_ms(1000);
         let message = if switch.is_high() { "Pin (D7) is HIGH" } else { "Pin (D7) is LOW"};
         uwriteln!(&mut serial, "{}", message).unwrap_infallible();
 
         let a0_value = a0.analog_read(&mut adc);
         uwriteln!(&mut serial, "Analog A0: {}", a0_value).unwrap_infallible();
+
+        match dht11.read() {
+            Ok(measurement) => {
+                uwriteln!(&mut serial, "DHT11 Temperature: {}°C", measurement.temperature).unwrap_infallible();
+                uwriteln!(&mut serial, "DHT11 Humidity: {}%", measurement.humidity).unwrap_infallible();
+            }
+            Err(e) => {
+                match e {
+                    embedded_dht_rs::SensorError::ChecksumMismatch => uwriteln!(&mut serial, "DHT11 Checksum Mismatch").unwrap_infallible(),
+                    embedded_dht_rs::SensorError::Timeout => uwriteln!(&mut serial, "DHT11 Timeout Error").unwrap_infallible(),
+                    embedded_dht_rs::SensorError::PinError => uwriteln!(&mut serial, "DHT11 Pin Error").unwrap_infallible(),
+                }
+            }
+            
+        }
+                arduino_hal::delay_ms(1000);
 
     }
 }
